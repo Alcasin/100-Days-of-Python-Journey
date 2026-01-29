8caa1ccc0aa903601d204fddb757c67a9e642abf
Alper Kırca <alpr.1496.6941@gmail.com>
2026-01-29 18:11:48 +0300

    Added exception handling to NATO Alphabet Project

--- Day-26/main.py
+++ Day-26/main.py
@@ -1,10 +1,31 @@
 import pandas
 
-df = pandas.read_csv("nato_phonetic_alphabet.csv")
-nato_dict =  {row.letter:row.code for (index, row) in df.iterrows()}
-user_word = input("Enter a word: ").upper()
-result = [nato_dict[letter] for letter in user_word if letter in nato_dict]
-print(result)
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+phonetic_dict =  {row.letter: row.code for (index, row) in data.iterrows()}
+
+continue_write = True
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
+
+
+# import pandas
+#
+# df = pandas.read_csv("nato_phonetic_alphabet.csv")
+# nato_dict =  {row.letter:row.code for (index, row) in df.iterrows()}
+# user_word = input("Enter a word: ").upper()
+# result = [nato_dict[letter] for letter in user_word if letter in nato_dict]
+# print(result)
 
 # import random
 # new_list = [new_item for item in list]
